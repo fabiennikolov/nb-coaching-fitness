eab02c05798bc88adcc50a42d015b95474d07438
frontend-alex <alexmmech@gmail.com>
2024-08-06 23:34:19 +0300

    initial-commit landing  page route

---
+++ resources/js/Pages/LandingPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div className='bg-r'>Welcome</div>
+  )
+}
+
+export default Welcome
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return Inertia::render('LandingPage', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
@@ -25,9 +25,13 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/users', function () {
+    return Inertia::render('Users/UserPage');
+});
+
+// Route::get('/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
