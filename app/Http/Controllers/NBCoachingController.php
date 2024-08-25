19a4443e822154fb59be082c8be2adf0568d83aa
Aleksandra <dimcaka@gmail.com>
2024-08-25 21:25:57 +0200

    save work

---
+++ app/Http/Controllers/NBCoachingController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NBCoachingController extends Controller
+{
+    public function index()
+    {
+                                
+    }
+}
