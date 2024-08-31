ecfaa1e3ae7007058c506dea5203ff904fda4225
Aleksandra <dimcaka@gmail.com>
2024-08-31 19:02:37 +0200

    make db seeders for users and roles & permissions

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $permissions = [
+            'register users',
+            'create table',
+            'edit table',
+            'read table',
+            'delete table',
+            'upload image',
+            'delete image',
+        ];
+
+        // Create permissions
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(['name' => $permission]);
+        }
+
+        // Define roles and their corresponding permissions
+        $rolesWithPermissions = [
+            'superadmin' => Permission::all(),
+            'admin' => [
+                'register users',
+                'create table',
+                'edit table',
+                'read table',
+                'delete table',
+                'upload image',
+                'delete image',
+            ],
+            'client' => [
+                'read table',
+                'edit table',
+                'upload image',
+            ],
+        ];
+
+        // Create roles and assign permissions
+        foreach ($rolesWithPermissions as $roleName => $rolePermissions) {
+            $role = Role::firstOrCreate(['name' => $roleName]);
+            if ($roleName === 'superadmin') {
+                $role->syncPermissions($rolePermissions); // Sync all permissions to superadmin
+            } else {
+                $role->syncPermissions($rolePermissions);
+            }
+        }
+    }
+    
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $superadminRole = Role::firstOrCreate(['name' => 'superadmin']);
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+
+        // Create users
+        $users = [
+            [
+                'name' => 'Alex',
+                'email' => 'dimcaka@gmail.com',
+                'password' => bcrypt('password'),
+                'role' => 'superadmin',
+            ],
+            [
+                'name' => 'Alex Mech',
+                'email' => 'alexmech@gmail.com',
+                'password' => bcrypt('password'),
+                'role' => 'superadmin',
+            ],
+            [
+                'name' => 'NBCoaching Fitness',
+                'email' => 'nbcoachingfitness@gmail.com',
+                'password' => bcrypt('password'),
+                'role' => 'admin',
+            ],
+        ];
+
+        // Loop through each user, create the user and assign the appropriate role
+        foreach ($users as $userData) {
+            $user = User::firstOrCreate(
+                ['email' => $userData['email']],
+                [
+                    'name' => $userData['name'],
+                    'password' => $userData['password'],
+                ]
+            );
+
+            // Assign the role to the user
+            $user->assignRole($userData['role']);
+        }
+    }
+}
