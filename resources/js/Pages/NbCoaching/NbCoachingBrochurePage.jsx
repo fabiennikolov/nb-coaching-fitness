4d436a3c8d270f2ee82c792d804707ed999d7359
frontend-alex <alexmmech@gmail.com>
2024-09-01 16:19:57 +0300

    snimki

--- resources/js/Pages/NbCoaching/NbCoachingBrochurePage.jsx
+++ resources/js/Pages/NbCoaching/NbCoachingBrochurePage.jsx
@@ -43,7 +43,7 @@ const NbCoachingBrochurePage = () => {
                     {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].map(
                         (_, id) => (
                             <img
-                                src={`/assets/brochureOne/05cdb2d5-93b2-4258-9a23-929be231661f-${id}.jpg`}
+                                src={`/assets/brochureOne/pic-${id + 1}.jpg`}
                                 alt={id}
                             />
                         )
