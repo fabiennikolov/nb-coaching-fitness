11733af33366c3a2078538a45d71c1c1aef9811a
frontend-alex <alexmmech@gmail.com>
2024-09-19 12:27:18 +0300

    cicki

--- resources/js/Pages/LandingPage.jsx
+++ resources/js/Pages/LandingPage.jsx
@@ -125,7 +125,7 @@ const Welcome = (props) => {
                                 <h1 className="text-3xl font-bold">
                                     Нямаш профил?
                                 </h1>
-                                <p>
+                                <p className="text-neutral-300">
                                     Влезни си в акаунта за да видиш цялата
                                     информация
                                 </p>
