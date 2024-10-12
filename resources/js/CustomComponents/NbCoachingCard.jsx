8bcaf958531643bdedce94739a5cfee1e434b809
Aleksandra <dimcaka@gmail.com>
2024-10-12 19:16:54 +0200

    remove blur

--- resources/js/CustomComponents/NbCoachingCard.jsx
+++ resources/js/CustomComponents/NbCoachingCard.jsx
@@ -22,7 +22,6 @@ const NbCoachingCard = ({ img, link, userPermissions, setBlur, blur }) => {
         <div className="flex-col-3 p-3 border border-neutral-800 group hover:border-neutral-300 rounded-md transition-all">
             <img
                 className={'w-full h-[200px] object-cover rounded-md'}
-                style={{ filter: (readBrochurePermission ? '': 'blur(3px)') }}
                 src={img}
             />
             <div className="flex-between">
